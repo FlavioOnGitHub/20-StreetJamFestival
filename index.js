6619cd827f2b8d4a57cf478a5469bed38d33703f
FlavioOnGitHub <flaviorotunno@gmail.com>
2023-06-07 10:47:05 +0200

    Added javascript to "animate" the nav and finished the css

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 window.onload = () => {
   const open = document.getElementById("burger-menu-open");
   const close = document.getElementById("burger-menu-close");
-
+  const toggle = document.getElementById("toggle");
 
   open.addEventListener("click", () => {
     open.style.display = "none";
     close.style.display = "block";
+    toggle.style.display = "block";
   });
 
   close.addEventListener("click", () => {
     close.style.display = "none";
     open.style.display = "block";
+    toggle.style.display = "none";
   });
 };
